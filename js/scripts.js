2e03f84ac6a8a2282e6b691285b9eb37638783c6
PSD <psdubowik@gmail.com>
2017-10-17 11:09:51 +0200

    Adding new variable and cleaning code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var votingButtons = document.getElementsByClassName("button");
+var votingButtonLen = votingButtons.length;
 
-for (var buttonNum = 0; buttonNum < votingButtons.length; buttonNum ++){
+for (var buttonNum = 0; buttonNum < votingButtonLen; buttonNum++){
     var vote = votingButtons[buttonNum].innerHTML;
     console.log(vote);
     alert(vote);
